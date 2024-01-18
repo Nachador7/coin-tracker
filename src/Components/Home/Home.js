fad3f4dbd33e3bc1a5bc842b4458f9f74ae088c4
Nachador7 <153395315+Nachador7@users.noreply.github.com>
2024-01-18 12:14:29 -0300

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <h1>
+              TRACK YOUR CYPTOCURRENCIES
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,17 @@
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <nav className="Navigation">
+            {/* <div className='logo'>Logo</div> */}
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    )
+
+}
+
+export default Navigation;
