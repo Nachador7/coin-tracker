b2a29192daa50fed12f36851d61e4321b7b7d690
Nachador7 <153395315+Nachador7@users.noreply.github.com>
2024-01-18 21:17:46 -0300

    changes

--- src/Components/Market/Market.js
+++ src/Components/Market/Market.js
@@ -8,14 +8,14 @@ const Market = () => {
     const data = Fetchdata();
 
     return (
-        <div className='market w-100 pl6'>
+        <div className='market w-100 pl6 pr6'>
             <h2>Market</h2>
             {data ? (
                 <div className="overflow-auto">
-                   <table className='f4 w-100 mw-100 center mr4' cellspacing='0'>
-    <thead>
+                   <table className='f4 w-100 mw-100 center mr4' cellSpacing='0'>
+     <thead className=''>
         <tr className='stripe-dark'>
-            <th className='fw6 tl pa3 ' style={{ paddingLeft: '137px' }}>Coin</th>
+            <th className='fw6 tl pa3' style={{ paddingLeft: '137px' }}>Coin</th>
             <th className='fw6 tl pa3'>Price</th>
             <th className='fw6 tl pa3'>24h Change</th>
             <th className='fw6 tl pa3'>Market Cap</th>
@@ -24,10 +24,10 @@ const Market = () => {
     <tbody className='lh-copy'>
         {data.slice(0, 30).map((coin, index) => (
             <tr className={index % 2 === 0 ? 'stripe-dark' : ''} key={index}>
-                <td className='pa3'><img src={coin.image} alt={coin.name} className='h3 w3 dib mr4 v-mid' />{coin.name}</td>
-                <td className='pa3'>${coin.current_price}</td>
-                <td className='pa3'>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-                <td className='pa3'>${coin.market_cap.toLocaleString()}</td>
+                <td className='pa3 bb b--white'><img src={coin.image} alt={coin.name} className='h3 w3 dib mr4 v-mid' />{coin.name}</td>
+                <td className='pa3 bb b--white'>${coin.current_price}</td>
+                <td className='pa3 bb b--white'>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+                <td className='pa3 bb b--white'>${coin.market_cap.toLocaleString()}</td>
             </tr>
         ))}
     </tbody>
